c51241025c1ab9676ad82c81e11bdd314d3a029c
Deano <dnoparker@gmail.com>
2019-06-26 20:52:31 +0100

    Clean up

--- engine.py
+++ engine.py
@@ -5,7 +5,6 @@ from map_objects.game_map import GameMap
 from render_functions import render_all, clear_all
 
 
-
 def main():
     screen_width = 80
     screen_height = 50
@@ -20,20 +19,19 @@ def main():
         'dark_wall': libtcod.Color(0, 0, 100),
         'dark_ground': libtcod.Color(50, 50, 150),
         'corridor': libtcod.Color(255, 0, 0)
-        }
+    }
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
     entities = [npc, player]
 
-
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+    libtcod.console_init_root(screen_width, screen_height, 'Roguelike', False)
 
     con = libtcod.console_new(screen_width, screen_height)
 
-    game_map = GameMap(map_width,map_height)
+    game_map = GameMap(map_width, map_height)
     game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
     key = libtcod.Key()
@@ -42,7 +40,7 @@ def main():
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        render_all(con,entities,game_map,screen_width,screen_height, colours)
+        render_all(con, entities, game_map, screen_width, screen_height, colours)
 
         libtcod.console_flush()
 
@@ -57,13 +55,13 @@ def main():
         if move:
             dx, dy = move
             if not game_map.is_blocked(player.x + dx, player.y + dy):
-                player.move(dx,dy)
+                player.move(dx, dy)
 
         if exit:
             return True
 
         if fullscreen:
-            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen()) 
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
             return True
 
