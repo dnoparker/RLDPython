25a1071f07183443f561d7fd241551af6733bbed
dnoparker <dnoparker@gmail.com>
2019-06-27 13:16:50 +0100

    Added explored param

--- map_objects/tile.py
+++ map_objects/tile.py
@@ -2,14 +2,15 @@ class Tile:
     """
     A tile on a map. It may or may not be blocked, and may or may not block sight.
     """
-    def __init__(self, blocked, corridor = False, room = False, block_sight = None):
+    def __init__(self, blocked, block_sight = None):
         self.blocked = blocked
-        self.corridor = corridor
-        self.room = room
+        self.corridor = False
+        self.room = False
         
         # By default, if a tile is blocked, it also blocks sight
         if block_sight is None:
             block_sight = blocked
         
         self.block_sight = block_sight
+        self.explored = False
     
